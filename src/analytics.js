5742365c0c4aeadbc35125ec734cff641a6bc1fe
Sungwoo Cho <sungwoocho2007@gmail.com>
2026-07-04 17:41:11 -0700

    security

--- src/analytics.js
+++ src/analytics.js
@@ -8,8 +8,12 @@
     path: location.pathname + location.search,
   };
 
+  // Version-pinned + subresource integrity: the browser refuses to run the
+  // script if its contents ever differ from this hash (CDN compromise guard).
   var s = document.createElement('script');
   s.async = true;
-  s.src = 'https://gc.zgo.at/count.js';
+  s.src = 'https://gc.zgo.at/count.v5.js';
+  s.integrity = 'sha384-atnOLvQb9t+jTSipvd75X2yginT4PjVbqDdlJAmxMm+wYElFmeR6EmLP5bYeoRVQ';
+  s.crossOrigin = 'anonymous';
   document.head.appendChild(s);
 })();
